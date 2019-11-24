676503b9b49ac9fef927a9c0731b9a517f1e9c36
bgasparotto <bruno.m.gasparotto@gmail.com>
2019-11-24 15:57:14 +0100

    Initial project setup.

---
+++ src/main/java/com/bgasparotto/hansardreader/HansardReaderApplication.java
@@ -0,0 +1,12 @@
+package com.bgasparotto.hansardreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansardReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HansardReaderApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bgasparotto/hansardreader/HansardReaderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bgasparotto.hansardreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansardReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
