c14a4bdf3c6e126bafcfeab8c40cadc3f33b95e1
bgasparotto <bruno.m.gasparotto@gmail.com>
2019-12-12 11:17:36 +0100

    Parameterised Kafka topic names and 3rd party API settings.

--- src/main/java/com/bgasparotto/hansardreader/messaging/consumer/RunUpdateConsumer.java
+++ src/main/java/com/bgasparotto/hansardreader/messaging/consumer/RunUpdateConsumer.java
@@ -1,6 +1,8 @@
 package com.bgasparotto.hansardreader.messaging.consumer;
 
+import com.bgasparotto.hansardreader.service.HansardConsumerService;
 import com.bgasparotto.scheduler.message.RunUpdate;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,11 +10,13 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class RunUpdateConsumer {
+    private final HansardConsumerService hansardConsumerService;
 
-    @KafkaListener(topics = "message.scheduler.run-hansard-update")
+    @KafkaListener(topics = "${topics.input.run-hansard-update}")
     public void consume(ConsumerRecord<String, RunUpdate> record) {
-        /* To be updated triggering the real API call. */
         log.info("Received command for updating Hansard data: [{}]", record.value());
+        hansardConsumerService.queryFeed();
     }
 }
